5fc459f5f93417024eb662f3da2d8f8181067b30
majestice_coder <142192726+Kapil9711@users.noreply.github.com>
2024-05-23 11:51:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const btn = document.querySelector('#btn');
+const input = document.querySelector('#ip');
+const output = document.querySelector('#output');
+
+
+function handleClick(){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			output.textContent = input.value;
+			resolve(input.value);
+		},2000);
+})
+}
+
+
+btn.addEventListener('click', ()=>{
+  hanldeClick().then(num => output.textContent = num);
+
+}
+					);
+
+
